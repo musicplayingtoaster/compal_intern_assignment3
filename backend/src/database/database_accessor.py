7e0ba2f04d902cb5173cccf20be0ee5141e65ecf
Liu. Alan (TPE) <Alan_Liu@compal.com>
2026-07-01 14:29:53 +0800

    fixed delete listener startup not found (compose yaml err) and potentially fixed db connection issues

--- backend/src/database/database_accessor.py
+++ backend/src/database/database_accessor.py
@@ -104,12 +104,12 @@ def create_lifespan(rabbitmq_listener):
         rediscache_sync_client = redis.Redis(connection_pool=sync_pool)
         rediscache_async_client = aioredis.Redis(connection_pool=async_pool)
 
+        postgres_sync_pool.open()
+        await postgres_async_pool.open()
+
         with next(get_pg_sync_conn()) as sync_conn:
             database.init_todo_list(conn_db=sync_conn)
         listener_task = asyncio.create_task(rabbitmq_listener())
-
-        postgres_sync_pool.open()
-        await postgres_async_pool.open()
 
         yield
 
