b44c432959f3fe0ae9e1a17368056e51076bad47
Liu. Alan (TPE) <Alan_Liu@compal.com>
2026-07-01 10:12:29 +0800

    nginx.conf cleanup and docker compose nginx static file mount fix

--- backend/src/resources/connections.py
+++ backend/src/resources/connections.py
@@ -1,6 +1,5 @@
 # connection paramters
 import os
-from typing import Dict, Callable, Any
 
 connection_params_db = {
     "host": os.environ.get('DB_HOST'), 
@@ -21,3 +20,9 @@ connection_params_rabbitmq = {
     "host": os.environ.get('RBMQ_HOST'),
     "port": int(os.environ.get('RBMQ_PORT')),
 }
+
+# testing purposes
+# connection_params_rabbitmq = {
+#     "host": "localhost",
+#     "port": 5672,
+# }
