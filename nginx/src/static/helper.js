fd02fd960b867f01d45d927bf150b563054b043b
Liu. Alan (TPE) <Alan_Liu@compal.com>
2026-07-02 17:14:41 +0800

    i am still an idiot and that fact hasnt changed

--- nginx/src/static/helper.js
+++ nginx/src/static/helper.js
@@ -141,6 +141,6 @@ function deleteSelf(id){
 function updateTodo(id, resolved){
     console.log("updating!", id)
     let parent_todo = document.getElementById(id.toString())
-    let checkbox = parent_todo.querySelector('input[name="resolved"]')
+    let checkbox = parent_todo.querySelector('input[name="resolve"]')
     if (resolved == 1) {checkbox.checked = true;} else {checkbox.checked = false;}
 }
